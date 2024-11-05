2a9c998db8a0fde902557af7751744328d051626
SidiKatana <joanakatana@gmail.com>
2024-11-05 13:36:09 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,10 @@
+# Ask the user for their name and store it in a variable called "name"
+name=input("What is your name?")
+
+# Ask the user for their age and store it in a variable called "age"
+age=input("How old are you?")
+
+# Ask the user for their location and store it in a variable called "location"
+location=input("What's your current location?")
+
+print("Hello {name}, i am {age} years old and live in {location}.")
